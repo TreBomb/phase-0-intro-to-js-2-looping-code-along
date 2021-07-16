af710d591e0bce21ea33701e6692749bab839830
Trestin Ishak <trestinishak@gmail.com>
2021-07-16 18:18:14 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function writeCards(names, event) {
+    let newArray = [];
+    for(let i=0; i < names.length; i++) {
+        let name = names[i]
+        console.log[names[i]]
+        let message = `Thank you, ${name}, for the wonderful ${event} gift!`;
+        newArray = [...newArray, message];
+    }
+    return newArray;
+}
+
+function countDown(start) {
+    while(start >= 0) {
+        console.log(start);
+        start--;
+    }
+}
